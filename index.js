52b8d3e866cdf398c09a9971e35f5c057d525cc0
Logan Negley <logannegley@gmail.com>
2020-04-10 19:54:10 -0500

    no changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const postsRouter = require('./routes/posts-router');
 
 const server= express();
 server.use(express.json());
+server.use('/api/posts', postsRouter);
 
 
-server.listen(3333, () => {
-console.log('listening on port 3333')
+server.listen(3000, () => {
+console.log('listening on port 3000')
 });
