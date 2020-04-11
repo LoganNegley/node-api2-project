1daa4fa6e9949285ea6cdc9d9644ae53b92c935f
Logan Negley <logannegley@gmail.com>
2020-04-10 20:20:18 -0500

    Post request made to add a post

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const db = require('../data/db');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-res.status(200).send('')
+// POST request making post
+router.post('/', (req, res) => {
+    if(!req.body.title || !req.body.contents){
+        return res.status(400).json({
+            errorMessage: 'Please provide title and contents for the post'
+        })
+    } 
+        db.insert(req.body)
+            .then( (post) => {
+                res.status(201).json(post)
+            })
+            .catch(error => {
+                console.log(error, 'error added post')
+                res.status(500).json({
+                    errorMessage: 'Error adding post'
+                })
+        })
 });
 
 module.exports = router;
