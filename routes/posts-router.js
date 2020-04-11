31c58ff59de2555695c481178b859a6355fd2268
Logan Negley <logannegley@gmail.com>
2020-04-11 01:45:48 -0500

    post a comment to a post

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -22,17 +22,51 @@ router.post('/', (req, res) => {
         })
 });
 
-// GET request to get all posts
-router.get('/', (req, res) =>{
-    const posts = db.find();
+// POST request with ID
+router.post('/:id/comments',(req, res)=>{
+    const id = req.params.id;
+    console.log(req.params.id)
 
-    if(posts){
-        res.json(posts)
-    } else{
-        res.status(500).json({
-            error:'The posts information could not be retieved'
+    if(!req.body.text){
+        return res.status(400).json({
+            errorMessage: 'Please provide text for the comment'
         })
     }
+        db.findById(id)
+        .then(post =>{
+            console.log(post)
+            if(!post){
+         return res.status(404).json({
+            message:'The post with the specified ID does not exist'
+            })
+        }else{
+            const newComment = {
+                text: req.body.text,
+                post_id: id
+            }
+           db.insertComment(newComment)
+            .then(comment =>{
+                res.status(200).json(comment)
+        })
+        .catch(error =>{
+            console.log(error, 'error creating new comment')
+        })
+    }
+     
+    })
+});
+
+// GET request to get all posts
+router.get('/', (req, res) =>{
+    db.find()
+        .then(response =>{
+            res.status(200).json(response)
+        })
+        .catch(error =>{
+            res.status(500).json({
+            error:'The posts information could not be retieved'
+        })
+    })
 });
 
 module.exports = router;
