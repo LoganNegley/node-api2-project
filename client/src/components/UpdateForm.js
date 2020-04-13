ce72013f05c9058c89b0ccd92a30f75f34cdd681
Logan Negley <logannegley@gmail.com>
2020-04-12 19:19:48 -0500

    updateForm getting post to update by ID

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -1,25 +1,29 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
+const initialPost = {
+    title:'',
+    content:''
+}
 
-const initialPost ={
-    title: '',
-    content: ''
-};
 
 function UpdateForm(props) {
-    const [post, setPost] = useState(initialPost);
+const [post, setPost] = useState(initialPost);
 
-// Find the post matching ID
+
+ // Find the post matching ID
     useEffect(()=>{
         const postToUpdate = props.postsList.find(item => (
             `${item.id}` === props.match.params.id
         ))
-        console.log(postToUpdate)
 
         if(postToUpdate){
             setPost(postToUpdate)
         }
-    },[]);
+    },[props.postsList]);
+
+
+
 
   return (
       <div className='updateForm'>
